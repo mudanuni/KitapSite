b6e28f074b47caf30da5ab6c2c766d5444846756
User <User@HP-NOTEBOOK>
2024-12-26 02:56:22 +0300

    Add project files.

---
+++ Site1.Master.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace KitapSite
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        sqlsinif bgl = new sqlsinif();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            SqlCommand komut = new SqlCommand("Select * from Kategoriler", bgl.baglanti());
+            SqlDataReader oku = komut.ExecuteReader();
+            DataList1.DataSource = oku;
+            DataList1.DataBind();
+       
+        }
+
+        protected void Menu1_MenuItemClick(object sender, MenuEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ sqlsinif.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace KitapSite
+{
+    public class sqlsinif
+    {
+        public SqlConnection baglanti() {
+
+            SqlConnection baglan = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; Initial Catalog=KitapSite Integrated Security = True;TrustServerCertificate=True;");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
